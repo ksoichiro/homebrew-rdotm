7697f9e111d2cd416e69f67d8531c5e6733a0662
ksoichiro <soichiro.kashima@gmail.com>
2014-08-30 19:32:16 +0900

    Bumped version to 0.5.0.

--- rdotm.rb
+++ rdotm.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Rdotm < Formula
   homepage "https://github.com/ksoichiro/rdotm"
-  version "0.4.0"
+  version "0.5.0"
 
   if Hardware.is_64_bit?
-    url "https://github.com/ksoichiro/rdotm/releases/download/v0.4.0/rdotm-0.4.0-bin-darwin-amd64.zip"
-    sha1 "6cce21f43ea4efb8ee0f8f7f152f1c10de03186a"
+    url "https://github.com/ksoichiro/rdotm/releases/download/v0.5.0/rdotm-0.5.0-bin-darwin-amd64.zip"
+    sha1 "fe6184be90a969d5e5b51513a6c81933577dec83"
   else
-    url "https://github.com/ksoichiro/rdotm/releases/download/v0.4.0/rdotm-0.4.0-bin-darwin-386.zip"
-    sha1 "a38c625fe754e5c6e35e1230e64d4d38692c1188"
+    url "https://github.com/ksoichiro/rdotm/releases/download/v0.5.0/rdotm-0.5.0-bin-darwin-386.zip"
+    sha1 "7eeb769a96853ad46f3ce645c5e57aeab79015a0"
   end
 
   def install
