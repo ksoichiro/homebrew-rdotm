47373329acc27eeab91154b54980a76441b6c565
Soichiro Kashima <soichiro.kashima@gmail.com>
2017-02-26 19:33:57 +0900

    Fix warnings

--- rdotm.rb
+++ rdotm.rb
@@ -4,12 +4,12 @@ class Rdotm < Formula
   homepage "https://github.com/ksoichiro/rdotm"
   version "0.5.0"
 
-  if Hardware.is_64_bit?
+  if Hardware::CPU.is_64_bit?
     url "https://github.com/ksoichiro/rdotm/releases/download/v0.5.0/rdotm-0.5.0-bin-darwin-amd64.zip"
-    sha1 "fe6184be90a969d5e5b51513a6c81933577dec83"
+    sha256 "1d3378a53cb21456f4efde9efcb18977901c35faf1d3d875b0d302a97578f24e"
   else
     url "https://github.com/ksoichiro/rdotm/releases/download/v0.5.0/rdotm-0.5.0-bin-darwin-386.zip"
-    sha1 "7eeb769a96853ad46f3ce645c5e57aeab79015a0"
+    sha256 "269eba98cb81ee03b35e9e7132dd3783a972a3458d9ce3f2c276d7e61fe818cd"
   end
 
   def install
